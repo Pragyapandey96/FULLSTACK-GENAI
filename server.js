5e284af341f97fc143920b9c96246c7ab37973bc
PragyaPandey96 <ppragya651@gmail.com>
2026-07-04 18:48:27 +0530

    register user

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const app = require('./src/app.js');
+const connectDB = require('./src/config/database.js');
+
+connectDB();
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+    
+})
