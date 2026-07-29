23406989f70dd21004062c01fb4e3155bb5e86ed
PragyaPandey96 <ppragya651@gmail.com>
2026-07-29 22:39:00 +0530

    home page

--- Frontend/src/features/landing/pages/Landing.jsx
+++ Frontend/src/features/landing/pages/Landing.jsx
@@ -1,19 +1,17 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-
+import Features from "../components/Features";
+import Footer from "../components/Footer";
 
 const Landing = () => {
-
-return (
-
-<>
-<Navbar/>
-<Hero/>
-</>
-
-)
-
-}
-
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
+};
 
 export default Landing;
