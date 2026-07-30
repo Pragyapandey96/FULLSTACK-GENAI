e72628357ad258bfb4b2d112a5e77f4e8d6c5bfd
PragyaPandey96 <ppragya651@gmail.com>
2026-07-31 01:13:24 +0530

    ready to deploy

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -90,7 +90,12 @@ async function loginUserController(req, res){
         { expiresIn: "1d" }
      )
 
-     res.cookie("token", token)
+     res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000, // 1 day
+});
 
      res.status(201).json({
         message: "User LoggedIn successfully",
@@ -123,7 +128,11 @@ async function loginUserController(req, res){
         await TokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
     res.status(200).json({
         message: "User logged out successfully"
